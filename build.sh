b2f8d1c6f6780045d54e1b5cb9ff2b0d9825a87a
Ramachandra Junior <ramachandrajr3@gmail.com>
2018-03-05 14:49:15 +0530

    Add switches to make our function call work
    We need to add some special switches to allow Javascript -> C++ functionality.

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-em++ -s WASM=1 multiply.cpp -o multiply.js
+em++ 	-s WASM=1 multiply.cpp -o multiply.js \
+	-s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]' \
+	-s EXPORTED_FUNCTIONS='["_main", "_multiply"]'
