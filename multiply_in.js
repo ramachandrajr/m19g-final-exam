02e10c4334f1afde592be80e26ba24b503a96331
Ramachandra Junior <ramachandrajr3@gmail.com>
2018-03-07 01:47:45 +0530

    Remove output() duplicates

--- multiply_in.js
+++ multiply_in.js
@@ -1,8 +1,3 @@
-// local print function
-function output(text) {
-  document.getElementById("output_area").append(text.toString() + "\n\n");
-}
-
 mergeInto(LibraryManager.library, {
   multiply_in_js: function (x, y) {
     output("multiplying in js : " + x + " * " + y);
